52b4c4ffcd09c0a75c6927e3c635c7e6af2f24a7
Tucker McCoy <tuckermmccoy@gmail.com>
2020-09-11 08:43:13 -0400

    Updated Terraform files

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,1 +1,14 @@
+provider "aws" {
+  region = var.region
+}
 
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    POC         = "tucker.m.mccoy@accenturefederal.com"
+    Environment = "test"
+    Purpose     = "IaC Pipeline Testing"
+  }
+}
